a6b6b13050cb5f8a0c509a884082d30cf0183b01
mervesenn <82107698+mervesenn@users.noreply.github.com>
2021-08-11 02:07:51 +0300

    Update hesapmakinesi.py

--- koşullu durumlar/hesapmakinesi.py
+++ koşullu durumlar/hesapmakinesi.py
@@ -11,13 +11,18 @@ işlemler:
 a = int(input("birinci sayı:"))
 b = int(input("ikinci sayı:"))
 işlem = input("işlemi giriniz:")
-if işlem=="1":
-    print("{} ile {} toplamı {} dir.".format(a,b,a+b))
-elif işlem=="2":
-    print("{} ile {} farkı {} dir.".format(a,b,a-b))
-elif işlem=="3":
-    print("{} ile {} çarpımı {} dir.".format(a,b,a*b))
-elif işlem=="4":
-    print("{} ile {} bölümü {} dir.".format(a,b,a/b))
+
+if islem == "1":
+    print("{} ile {} toplamı {} dir.".format(a, b, a + b))
+    
+elif islem == "2":
+    print("{} ile {} farkı {} dir.".format(a, b, a - b))
+    
+elif islem == "3":
+    print("{} ile {} çarpımı {} dir.".format(a, b, a * b))
+    
+elif islem == "4":
+    print("{} ile {} bölümü {} dir.".format(a, b, a / b))
+    
 else:
     print("geçersiz işlem.")
