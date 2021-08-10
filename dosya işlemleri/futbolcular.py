6c38ad2c25fe101b53d29acac5e273f22de440a6
mervesenn <82107698+mervesenn@users.noreply.github.com>
2021-08-11 00:22:41 +0300

    Update futbolcular.py

--- dosya işlemleri/futbolcular.py
+++ dosya işlemleri/futbolcular.py
@@ -1,23 +1,30 @@
-with open("futbolcular.txt","r",encoding="utf-8") as file:
+with open("futbolcular.txt","r",encoding = "utf-8") as file:
     gs = list()
     fb = list()
     bjk = list()
-    for satır in file:
-        satır = satır [:-1]
-        satırelemanları = satır.split(",")
-        if(satırelemanları[1]== "Fenerbahçe"):
-            fb.append(satır + "\n")
-        elif(satırelemanları[1]=="Galatasaray"):
-            gs.append(satır + "\n")
+    
+    for satir in file:
+        satir = satir [:-1]
+        satirelemanları = satir.split(",")
+        
+        if(satirelemanları[1] == "Fenerbahçe"):
+            fb.append(satir + "\n")
+            
+        elif(satirelemanlari[1] == "Galatasaray"):
+            gs.append(satir + "\n")
+            
         else:
-            bjk.append(satır + "\n")
-    with open("gs.txt","w",encoding="utf-8") as file1:
+            bjk.append(satir + "\n")
+            
+    with open("gs.txt","w",encoding = "utf-8") as file1:
         for i in gs:
             file1.write(i)
-    with open("fb.txt","w",encoding="utf-8") as file2:
+            
+    with open("fb.txt","w",encoding = "utf-8") as file2:
         for i in fb:
             file2.write(i)
-    with open("bjk.txt","w",encoding="utf-8") as file3:
+            
+    with open("bjk.txt","w",encoding = "utf-8") as file3:
         for i in bjk:
             file3.write(i)
 
