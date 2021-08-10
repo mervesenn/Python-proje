33a4e516bb50f4139f335274d40c6775f6e12141
mervesenn <82107698+mervesenn@users.noreply.github.com>
2021-08-11 00:36:47 +0300

    Update decorator-proje.py

--- decoratorler/decorator-proje.py
+++ decoratorler/decorator-proje.py
@@ -1,28 +1,36 @@
 def ekstra(fonksiyon):
-    def ekstraözellik():
+    def ekstraozellik():
         print("Mükemmel sayılar...")
-        for sayı in range(1,1001):
+        
+        for sayi in range(1,1001):
             toplam = 0
             i = 1
-            while (i < sayı):
-                if (sayı % i == 0):
+            
+            while (i < sayi):
+                if (sayi % i == 0):
                     toplam += i
 
                 i += 1
-            if (toplam == sayı):
-                print(sayı)
+            if (toplam == sayi):
+                print(sayi)
+                
         fonksiyon()
-    return ekstraözellik
+    return ekstraozellik
+
 @ekstra
-def asalsayılar():
+def asalsayilar():
     print("Asal sayılar...")
-    for sayı in range(2,1001):
+    
+    for sayi in range(2,1001):
         i = 2
         say = 0
-        while (i < sayı):
-            if (sayı % i == 0):
+        
+        while (i < sayi):
+            if (sayi % i == 0):
                 say += 1
+                
             i += 1
         if (say == 0):
-            print(sayı)
-asalsayılar()
+            print(sayi)
+            
+asalsayilar()
