b76f74d2f8a1861b2f8f4d50fc39c17ef6353e07
mervesenn <82107698+mervesenn@users.noreply.github.com>
2021-08-11 00:28:52 +0300

    Update nothesaplama.py

--- dosya işlemleri/nothesaplama.py
+++ dosya işlemleri/nothesaplama.py
@@ -18,22 +18,29 @@ def not_hesapla(satir):
     son_not = not1 * (3/10) + not2 * (3/10) + not3 * (4/10)
 
     if (son_not >= 90):
-
         harf = "AA"
+        
     elif (son_not >= 85):
         harf = "BA"
+        
     elif (son_not >= 80):
         harf = "BB"
+        
     elif (son_not >= 75):
         harf = "CB"
+        
     elif (son_not >= 70):
         harf = "CC"
+        
     elif (son_not >= 65):
         harf = "DC"
+        
     elif (son_not >= 60):
         harf = "DD"
+        
     elif (son_not >= 55):
         harf = "FD"
+        
     else:
         harf = "FF"
 
