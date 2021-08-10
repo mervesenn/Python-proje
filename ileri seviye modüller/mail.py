b19c0669096a9a7977388453ae6057cd56cf5ddf
mervesenn <82107698+mervesenn@users.noreply.github.com>
2021-08-11 01:32:43 +0300

    Update mail.py

--- ileri seviye modüller/mail.py
+++ ileri seviye modüller/mail.py
@@ -2,29 +2,32 @@ import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 import sys
+
 mesaj = MIMEMultipart()
 email = "mervesen1095@gmail.com"
-emailşifre = ""
+emailsifre = ""
 mesaj ["From"] = "mervesen1095@gmail.com"
 mailatilacaklar = ["mervesen1095@gmail.com"]
 mesaj ["Subject"] = "Toplu mesaj"
 
-yazı = """
+yazi = """
 smtp ile mesaj gönderme
 """
-mesajgövdesi = MIMEText(yazı,"plain")
-mesaj.attach(mesajgövdesi)
+mesajgovdesi = MIMEText(yazi, "plain")
+mesaj.attach(mesajgovdesi)
 
 try:
     mail = smtplib.SMTP("smtp.gmail.com",587)
     mail.ehlo()
     mail.starttls()
-    mail.login("mervesen1095@gmail.com","")
+    mail.login("mervesen1095@gmail.com", "")
+    
     for herbirmail in mailatilacaklar:
-        mail.sendmail(mesaj["From"],herbirmail,mesaj.as_string())
+        mail.sendmail(mesaj["From"], herbirmail, mesaj.as_string())
         print("{} adresine mail gönderildi..")
     print("Mailler başarıyla gönderildi..")
     mail.close()
+    
 except:
     sys.stderr.write("Bir sorun oluştu...")
     sys.stderr.flush()
