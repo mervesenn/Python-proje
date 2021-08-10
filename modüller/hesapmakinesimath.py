63f500c4de77e9751d8167894a4a180d6cd190e6
mervesenn <82107698+mervesenn@users.noreply.github.com>
2021-08-11 00:12:36 +0300

    Update hesapmakinesimath.py

--- modüller/hesapmakinesimath.py
+++ modüller/hesapmakinesimath.py
@@ -15,26 +15,33 @@ print("""*************
 3.işlem: çarpma işlemi
 4.işlem: bölme işlemi
 ************""")
+
+
 while True:
     islem = input("işlem seçiniz:")
     if (islem == "q"):
         print("tebrikler çıkış yaptınız")
         break
+        
     elif (islem == "1"):
         a = int(input("birinci sayı:"))
         b = int(input("ikinci sayı:"))
         print(toplama(a,b))
+        
     elif (islem == "2"):
         a = int(input("birinci sayı:"))
         b = int(input("ikinci sayı:"))
         print(cıkarma(a,b))
+        
     elif (islem == "3"):
         a = int(input("birinci sayı:"))
         b = int(input("ikinci sayı:"))
         print(carpma(a,b))
+        
     elif (islem == "4"):
         a = int(input("birinci sayı:"))
         b = int(input("ikinci sayı:"))
         print(bölme(a,b))
+        
     else:
         print("geçersiz işlem yaptınız lütfen tekrar deneyiniz")
