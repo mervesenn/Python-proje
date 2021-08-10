93cc9d045af46676c24fda19be5d6a6b2af8b6dc
mervesenn <82107698+mervesenn@users.noreply.github.com>
2021-08-11 01:18:31 +0300

    Update sysmodülü.py

--- ileri seviye modüller/sys modülü/sysmodülü.py
+++ ileri seviye modüller/sys modülü/sysmodülü.py
@@ -1,14 +1,18 @@
 import sys
-def kökbulma(a,b,c):
-    delta = b ** 2 -4*a*c
-    if (delta<0):
+
+def kokbulma(a,b,c):
+    delta = b ** 2 -4 * a * c
+    if (delta < 0):
         print("Reel kök yok")
+        
     else:
-        x1 = (-b-delta**0.5) / (2*a)
-        x2 = (-b+delta**0.5) / (2*a)
-        return (x1,x2)
+        x1 = (-b - delta ** 0.5) / (2 * a)
+        x2 = (-b + delta ** 0.5) / (2 * a)
+        return (x1, x2)
+    
 if len(sys.argv) == 4:
-    print("Kök bulma",kökbulma(int(sys.argv[1]),int(sys.argv[2]),int(sys.argv[3])))
+    print("Kök bulma", kokbulma(int(sys.argv[1]), int(sys.argv[2]), int(sys.argv[3])))
+    
 else:
     sys.stderr.write("Lütfen doğru değerler girin")
     sys.stderr.flush()
