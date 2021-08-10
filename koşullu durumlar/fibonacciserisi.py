5ff6fd5ebb5fa0ab7e3085cb1f52d5d0f79fcb47
mervesenn <82107698+mervesenn@users.noreply.github.com>
2021-08-11 01:53:44 +0300

    Update fibonacciserisi.py

--- koşullu durumlar/fibonacciserisi.py
+++ koşullu durumlar/fibonacciserisi.py
@@ -7,9 +7,9 @@ fibonacci serisi yeni bir sayıyı önceki iki sayının toplamı şeklinde olu�
 a = 1
 b = 1
 
-fibonacci = [a,b]
+fibonacci = [a, b]
 for i in range(10):
-    a,b = b,a+b
-    print("a:",a,"b",b)
+    a , b = b, a + b
+    print("a:", a,"b", b)
     fibonacci.append(b)
 print(fibonacci)
