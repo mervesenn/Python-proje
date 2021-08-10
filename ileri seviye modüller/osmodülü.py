4c768f5b51f07e4586e5190c216304622b4c2d43
mervesenn <82107698+mervesenn@users.noreply.github.com>
2021-08-11 01:35:20 +0300

    Update osmodülü.py

--- ileri seviye modüller/osmodülü.py
+++ ileri seviye modüller/osmodülü.py
@@ -1,12 +1,13 @@
 import os
-def dosyayazdır(isim,belge):
+
+def dosyayazdir(isim, belge):
     dosyalar = []
-    with open("{}".format(belge),"r+",encoding = "utf-8") as file:
+    with open("{}".format(belge), "r+",encoding = "utf-8") as file:
         for i in file:
             i = i.strip("\n")
             dosyalar.append(i)
         if not (isim in dosyalar):
-            file.write(isim+"\n")
+            file.write(isim + "\n")
 dosyalar = os.walk("C:\\")
 
 file1 = open("C://Users/Hp/Desktop/txtdosyaları.txt","a+")
@@ -17,12 +18,12 @@ file1.close()
 file2.close()
 file3.close()
 
-for kyolu,kisimleri,disimleri in dosyalar:
+for kyolu, kisimleri, disimleri in dosyalar:
     for i in disimleri:
         if i.endswith(".txt"):
-            dosyayazdır(kyolu + i,"C://Users/Hp/Desktop/txtdosyaları.txt")
+            dosyayazdir(kyolu + i,"C://Users/Hp/Desktop/txtdosyaları.txt")
         elif i.endswith(".mp4"):
-            dosyayazdır(kyolu + i,"C://Users/Hp/Desktop/mp4dosyaları.txt")
+            dosyayazdir(kyolu + i,"C://Users/Hp/Desktop/mp4dosyaları.txt")
         elif i.endswith(".pdf"):
-            dosyayazdır(kyolu + i,"C://Users/Hp/Desktop/pdfdosyaları.txt")
+            dosyayazdir(kyolu + i,"C://Users/Hp/Desktop/pdfdosyaları.txt")
 
