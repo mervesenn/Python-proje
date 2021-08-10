4212eb588a7465387f6b69cbf99504123c20d03e
mervesenn <82107698+mervesenn@users.noreply.github.com>
2021-08-11 00:58:29 +0300

    Update osmodül.py

--- ileri seviye modüller/os modülü/osmodül.py
+++ ileri seviye modüller/os modülü/osmodül.py
@@ -1,13 +1,15 @@
 import os
 from datetime import datetime
+
 #print(os.getcwd())
 #print(datetime.fromtimestamp(os.stat("test.txt").st_mtime))
 
-for klasöryolu,klasörisimleri,dosyaisimleri in os.walk("C:/Users/Hp/Desktop"):
-    #print("klasör yolu",klasöryolu)
-    #print("klasör isimleri", klasörisimleri)
+for klasoryolu, klasorisimleri, dosyaisimleri in os.walk("C:/Users/Hp/Desktop"):
+    #print("klasör yolu", klasoryolu)
+    #print("klasör isimleri", klasorisimleri)
     #print("dosya isimleri", dosyaisimleri)
     #print("--------------------------")
+    
     for i in dosyaisimleri:
         if (i.endswith(".txt")):
             print(i)
