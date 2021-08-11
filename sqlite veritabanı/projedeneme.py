ee2867950ea0144466dbd8b7ccd7669e2e160c73
mervesenn <82107698+mervesenn@users.noreply.github.com>
2021-08-11 16:17:31 +0300

    Update projedeneme.py

--- sqlite veritabanı/projedeneme.py
+++ sqlite veritabanı/projedeneme.py
@@ -1,4 +1,4 @@
-from kütüphane import *
+from kutuphane import *
 print("""*********************
 Kütüphane Programına Hoşgeldiniz
 İşlemler:
@@ -11,45 +11,51 @@ Kütüphane Programına Hoşgeldiniz
 ***********************
 """)
 
-kütüphane = Kütüphane()
+kutuphane = Kutuphane()
 
 while True:
-    işlem = input("Yapacağınız işlem:")
-    if (işlem == "q"):
+    islem = input("Yapacağınız işlem:")
+    if (islem == "q"):
         print("program sonlandırılıyor...")
         print("yine bekleriz.")
         break
-    elif (işlem == "1"):
-        kütüphane.kitaplarıgöster()
-    elif (işlem == "2"):
+        
+    elif (islem == "1"):
+        kutuphane.kitaplarigoster()
+        
+    elif (islem == "2"):
         isim = input("Hangi kitabı istiyorsunuz?")
         print("Kitap sorgulanıyor..")
         time.sleep(2)
-        kütüphane.kitapsorgula(isim)
-    elif (işlem == "3"):
+        kutuphane.kitapsorgula(isim)
+        
+    elif (islem == "3"):
         isim = input("İsim:")
         yazar = input("Yazar:")
-        yayınevi = input("Yayınevi:")
-        tür = input("Türü:")
-        baskı = int(input("Baskı:"))
-        yenikitap = Kitap(isim,yazar,yayınevi,tür,baskı)
+        yayinevi = input("Yayınevi:")
+        tur = input("Türü:")
+        baski = int(input("Baskı:"))
+        yenikitap = Kitap(isim,yazar,yayinevi,tur,baski)
         print("Kitap ekleniyor...")
         time.sleep(2)
-        kütüphane.kitapekle(yenikitap)
+        kutuphane.kitapekle(yenikitap)
         print("Kitap eklendi...")
-    elif (işlem == "4"):
+        
+    elif (islem == "4"):
         isim = input("Hangi kitabı silmek istiyorsunuz?")
         cevap = input("Emin misiniz? (E/H)")
         if (cevap == "E"):
             print("Kitap siliniyor...")
             time.sleep(2)
-            kütüphane.kitapsil(isim)
+            kutuphane.kitapsil(isim)
             print("Kitap silindi...")
-    elif (işlem == "5"):
+            
+    elif (islem == "5"):
         isim = input("Hangi kitabın baskısını yükseltmek istiyorsunuz?")
         print("Baskı yükseltiliyor...")
         time.sleep(2)
-        kütüphane.baskıyükselt(isim)
+        kutuphane.baskiyükselt(isim)
         print("Baskı yükseltildi...")
+        
     else:
         print("geçersiz işlem")
