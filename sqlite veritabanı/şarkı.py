c9db55b7df1b2afba2942d6c9d1cde1cb1c1ad07
mervesenn <82107698+mervesenn@users.noreply.github.com>
2021-08-11 16:42:29 +0300

    Update şarkı.py

--- sqlite veritabanı/şarkı.py
+++ sqlite veritabanı/şarkı.py
@@ -1,80 +1,92 @@
 import sqlite3
 import time
 
-class Şarkı():
-    def __init__(self,şarkıismi,sanatçı,albüm,prodüksiyonşirketi,dakika,saniye):
-        self.şarkıismi = şarkıismi
-        self.sanatçı = sanatçı
-        self.albüm = albüm
-        self.prodüksiyonşirketi = prodüksiyonşirketi
+class Sarki():
+    def __init__(self,sarkiismi,sanatci,album,produksiyonsirketi,dakika,saniye):
+        self.sarkiismi = sarkiismi
+        self.sanatci = sanatci
+        self.album = album
+        self.produksiyonsirketi = produksiyonsirketi
         self.dakika = dakika
         self.saniye = saniye
-        self.süre = str(self.dakika) + ":" + str(self.saniye)
+        self.sure = str(self.dakika) + ":" + str(self.saniye)
+        
     def __str__(self):
-        return "{}/ {}/ {}/ {}/ {}\n".format(self.şarkıismi,self.sanatçı,self.albüm,self.prodüksiyonşirketi,self.dakika,self.saniye)
-class Kütüphane():
+        return "{}/ {}/ {}/ {}/ {}\n".format(self.sarkiismi,self.sanatci,self.album,self.produksiyonsirketi,self.dakika,self.saniye)
+    
+class Kutuphane():
     def __init__(self):
-        self.baglantı = sqlite3.connect("şarkıkütüphanesi.db")
-        self.imleç = self.baglantı.cursor()
-        self.imleç.execute("Create Table If not exists Şarkılar(İsim TEXT,Sanatçı TEXT,Albüm TEXT,Prodüksiyonşirketi TEXT,Dakikasüre INT,Saniyesüre INT)")
-        self.baglantı.commit()
-    def baglantıekes(self):
-        self.baglantı.close()
-    def bilgilerigöster(self):
-        self.imleç.execute("Select * From Şarkılar")
-        a = self.imleç.fetchall()
+        self.baglanti = sqlite3.connect("şarkıkütüphanesi.db")
+        self.imlec = self.baglanti.cursor()
+        self.imlec.execute("Create Table If not exists Sarkilar(İsim TEXT,Sanatci TEXT,Album TEXT,Produksiyonsirketi TEXT,Dakikasüre INT,Saniyesüre INT)")
+        self.baglanti.commit()
+    def baglantikes(self):
+        self.baglanti.close()
+    def bilgilerigoster(self):
+        self.imlec.execute("Select * From Sarkilar")
+        a = self.imlec.fetchall()
         print("(Sıralama: Şarkı ismi/ Sanatçı/ Albüm/ Prodüksiyon şirketi/ Şarkı süresi)\n")
         for i in a:
-            print(Şarkı(i[0],i[1],i[2],i[3],i[4],i[5]))
-    def şarkıekle(self):
+            print(Sarki(i[0],i[1],i[2],i[3],i[4],i[5]))
+            
+    def sarkiekle(self):
         print("Lütfen sizden istenilen bilgileri uygun yerlere yazın.)")
         time.sleep(2)
-        şarkıismi = input("Şarkı ismi:")
-        if (şarkıismi == "q"):
+        sarkiismi = input("Şarkı ismi:")
+        if (sarkiismi == "q"):
             print("Geri dönülüyor...")
             time.sleep(2)
             return
-        sanatçı = str(input("Sanatçı adı:"))
-        if(şarkıismi == "q"):
+        
+        sanatci = str(input("Sanatçı adı:"))
+        if(sarkiismi == "q"):
             print("Geri dönülüyor...")
             time.sleep(2)
             return
-        albüm = str(input("Albüm adı:"))
-        if (şarkıismi == "q"):
+        
+        album = str(input("Albüm adı:"))
+        if (sarkiismi == "q"):
             print("Geri dönülüyor...")
             time.sleep(2)
             return
-        prodüksiyonşirketi = str(input("Prodüksiyon şirketi:"))
-        if (prodüksiyonşirketi == "q"):
+        
+        produksiyonsirketi = str(input("Prodüksiyon şirketi:"))
+        if (produksiyonsirketi == "q"):
             print("Geri dönülüyor...")
             time.sleep(2)
             return
+        
         try:
-            şarkısüresidakika = int(input("Şarkı süresi(dakika):"))
-            if (şarkıismi == "q"):
+            sarkisuresidakika = int(input("Şarkı süresi(dakika):"))
+            if (sarkiismi == "q"):
                 print("Geri dönülüyor...")
                 time.sleep(2)
                 return
-            şarkısüresisaniye = int(input("Şarkı süresi(saniye):"))
-            if (şarkıismi == "q"):
+            
+            sarkisuresisaniye = int(input("Şarkı süresi(saniye):"))
+            if (sarkiismi == "q"):
                 print("Geri dönülüyor...")
                 time.sleep(2)
                 return
+            
         except (ValueError):
             print("Lütfen rakam giriniz.\n")
-            return Kütüphane.şarkıekle()
+            return Kutuphane.sarkiekle()
+        
         print("Şarkı ekleniyor...")
         time.sleep(2)
-        self.imleç.execute("Insert into Şarkılar Values(?,?,?,?,?,?)",(şarkıismi,sanatçı,albüm,prodüksiyonşirketi,şarkısüresidakika,şarkısüresisaniye))
-        self.baglantı.commit()
+        self.imlec.execute("Insert into Sarkilar Values(?,?,?,?,?,?)",(sarkiismi,sanatci,album,produksiyonsirketi,sarkisuresidakika,sarkisuresisaniye))
+        self.baglanti.commit()
         print("Şarkı eklendi...")
-    def şarkısil(self):
+        
+    def sarkisil(self):
         print("Şarkılar:")
-        self.imleç.execute("Select İsim From Şarkılar")
-        a = self.imleç.fetchall()
+        self.imlec.execute("Select İsim From Sarkilar")
+        a = self.imlec.fetchall()
         if (len(a) == 0):
             print("Şarkı listesi boş")
             return
+        
         for i in a:
             print("-" + i[0])
         for i in a:
@@ -83,18 +95,21 @@ class Kütüphane():
                 print("Geri dönülüyor...")
                 time.sleep(2)
                 return
+            
             elif (i[0] == isim):
                 print("Şarkı siliniyor...")
                 time.sleep(2)
-                self.imleç.execute("Delete From Şarkılar Where İsim = ?",(isim,))
-                self.baglantı.commit()
+                self.imlec.execute("Delete From Sarkilar Where İsim = ?",(isim,))
+                self.baglanti.commit()
                 print("Şarkı silindi.")
+                
             elif (i[0] != isim):
                 print("Böyle bir isimde şarkı kütüphanede yok.")
                 continue
-    def toplamsüre(self):
-        self.imleç.execute("Select Dakikasüre, Saniyesüre From Şarkılar")
-        a = self.imleç.fetchall()
+                
+    def toplamsure(self):
+        self.imlec.execute("Select Dakikasüre, Saniyesüre From Sarkilar")
+        a = self.imlec.fetchall()
         geneltoplamdakika = 0
         geneltoplamsaniye = 0
         for i in a:
@@ -110,31 +125,37 @@ class Kütüphane():
                 return False
     def yardım(self):
         print("İşlemler:\n\tKütüphanedeki şarkıları görmek için:(Şarkıları göster),\n\tKütüphaneye şarkı eklemek için:(Şarkı ekle),\n\tKütüphaneden şarkı silmek için:(Şarkı sil),\n\tKütüphanedeki toplam şarkı süresini görmek için: (Toplam süreyi göster)yazınız.")
-kütüphane = Kütüphane()
+kutuphane = Kutuphane()
 print("\tŞarkı kütüphanesi v1.0")
 time.sleep(1)
 print("\n\t Hoşgeldiniz\n\n(Yardım almak istiyorsanız(h), çıkmak istiyorsanız(q) tuşuna basın.)")
 while True:
     print("******************************\n")
-    işlem = input("----->")
+    islem = input("----->")
     print("-------------------------\nİşlem yapılıyor,lütfen bekleyiniz...\n---------------------------")
     time.sleep(2)
-    if(işlem == "q" or işlem == "Q"):
+    if(islem == "q" or islem == "Q"):
         print("Uygulamadan çıkılıyor...")
         time.sleep(2)
         print("\t Hoşçakalın")
         break
-    elif(işlem == "h" or işlem == "H"):
-        kütüphane.yardım()
-    elif(işlem == "Şarkıları göster" or işlem == "ŞARKILARI GÖSTER" or işlem == "şarkıları göster"):
+        
+    elif(islem == "h" or islem == "H"):
+        kutuphane.yardim()
+        
+    elif(islem == "Şarkıları göster" or islem == "ŞARKILARI GÖSTER" or islem == "şarkıları göster"):
         print("Şarkılar:")
-        kütüphane.bilgilerigöster()
-    elif(işlem == "Şarkı ekle" or işlem == "ŞARKI EKLE" or işlem == "şarkı ekle"):
-        kütüphane.şarkıekle()
-    elif(işlem == "Şarkı sil" or işlem == "ŞARKI SİL" or işlem == "şarkı sil"):
-        kütüphane.şarkısil()
-    elif(işlem  == "Toplam süreyi göster" or işlem == "TOPLAM SÜREYİ GÖSTER" or işlem == "toplam süreyi göster"):
-        kütüphane.toplamsüre()
+        kutuphane.bilgilerigoster()
+        
+    elif(islem == "Şarkı ekle" or islem == "ŞARKI EKLE" or islem == "şarkı ekle"):
+        kutuphane.sarkiekle()
+        
+    elif(islem == "Şarkı sil" or işlem == "ŞARKI SİL" or islem == "şarkı sil"):
+        kutuphane.sarkisil()
+        
+    elif(islem  == "Toplam süreyi göster" or islem == "TOPLAM SÜREYİ GÖSTER" or islem == "toplam süreyi göster"):
+        kutuphane.toplamsure()
+         
     else:
         print("Geçersiz işlem")
 
