c81df7be17b5aa570e0058892494e8165b81524b
mervesenn <82107698+mervesenn@users.noreply.github.com>
2021-08-11 16:05:00 +0300

    Update pyqt6.py

--- pyqt5/pyqt6.py
+++ pyqt5/pyqt6.py
@@ -1,29 +1,35 @@
 import sys
 from PyQt5 import QtWidgets
+
 class Pencere(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
         self.init_ui()
+        
     def init_ui(self):
-        self.yazıalanı = QtWidgets.QLineEdit()
+        self.yazialani = QtWidgets.QLineEdit()
         self.temizle = QtWidgets.QPushButton("Temizle")
-        self.yazdır = QtWidgets.QPushButton("Yazdır")
+        self.yazdir = QtWidgets.QPushButton("Yazdır")
         v_box = QtWidgets.QVBoxLayout()
-        v_box.addWidget(self.yazıalanı)
+        v_box.addWidget(self.yazialani)
         v_box.addWidget(self.temizle)
-        v_box.addWidget(self.yazdır)
+        v_box.addWidget(self.yazdir)
         v_box.addStretch()
         self.setLayout(v_box)
         self.temizle.clicked.connect(self.click)
-        self.yazdır.clicked.connect(self.click)
+        self.yazdir.clicked.connect(self.click)
         self.show()
+        
     def click(self):
         sender = self.sender()
         if sender.text() == "Temizle":
-            self.yazıalanı.clear()
+            self.yazialani.clear()
+            
         else:
-            print(self.yazıalanı.text())
+            print(self.yazialani.text())
 
 app = QtWidgets.QApplication(sys.argv)
+
 pencere = Pencere()
+
 sys.exit(app.exec())
