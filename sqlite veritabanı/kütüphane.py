3c58c535ef5f33f5b774b93570f9c2f2c3714d8c
mervesenn <82107698+mervesenn@users.noreply.github.com>
2021-08-11 16:13:28 +0300

    Update kütüphane.py

--- sqlite veritabanı/kütüphane.py
+++ sqlite veritabanı/kütüphane.py
@@ -1,36 +1,43 @@
 import sqlite3
 import time
+
 class Kitap():
-    def __init__(self,isim,yazar,yayınevi,tür,baskı):
+    def __init__(self,isim,yazar,yayinevi,tur,baski):
         self.isim = isim
         self.yazar = yazar
-        self.yayınevi = yayınevi
-        self.tür = tür
-        self.baskı = baskı
+        self.yayinevi = yayinevi
+        self.tur = tur
+        self.baski = baski
+        
     def __str__(self):
-        return "Kitap ismi: {}\nYazar: {}\nYayınevi: {}\nTür: {}\nBaskı: {}\n".format(self.isim,self.yazar,self.yayınevi,self.tür,self.baskı)
+        return "Kitap ismi: {}\nYazar: {}\nYayınevi: {}\nTür: {}\nBaskı: {}\n".format(self.isim, self.yazar, self.yayinevi, self.tur, self.baski)
 
-class Kütüphane():
+class Kutuphane():
     def __init__(self):
-        self.baglantıolustur()
-    def baglantıolustur(self):
-        self.baglantı = sqlite3.connect("kütüphane.db")
-        self.cursor = self.baglantı.cursor()
-        sorgu ="Create Table If not exists kitaplar(isim TEXT,yazar TEXT,yayınevi TEXT,tür TEXT,baskı INT)"
+        self.baglantiolustur()
+        
+    def baglantiolustur(self):
+        self.baglanti = sqlite3.connect("kütüphane.db")
+        self.cursor = self.baglanti.cursor()
+        sorgu ="Create Table If not exists kitaplar(isim TEXT, yazar TEXT, yayinevi TEXT, tur TEXT, baski INT)"
         self.cursor.execute(sorgu)
-        self.baglantı.commit()
-    def baglantıyıkes(self):
-        self.baglantı.close()
-    def kitaplarıgöster(self):
+        self.baglanti.commit()
+        
+    def baglantiyikes(self):
+        self.baglanti.close()
+        
+    def kitaplarigoster(self):
         sorgu = "Select * From kitaplar"
         self.cursor.execute(sorgu)
         kitaplar = self.cursor.fetchall()
         if (len(kitaplar) == 0):
             print("Kütüphanede kitap bulunmuyor..")
+            
         else:
             for i in  kitaplar:
                 kitap = Kitap(i[0],i[1],i[2],i[3],i[4])
                 print(kitap)
+                
     def kitapsorgula(self,isim):
         sorgu = "Select * From kitaplar where isim = ?"
         self.cursor.execute(sorgu,(isim,))
@@ -40,26 +47,30 @@ class Kütüphane():
         else:
             kitap = Kitap(kitaplar[0][0],kitaplar[0][1],kitaplar[0][2],kitaplar[0][3],kitaplar[0][4])
             print(kitap)
+            
     def kitapekle(self,kitap):
         sorgu = "Insert into kitaplar Values(?,?,?,?,?)"
-        self.cursor.execute(sorgu,(kitap.isim,kitap.yazar,kitap.yayınevi,kitap.tür,kitap.baskı))
-        self.baglantı.commit()
+        self.cursor.execute(sorgu,(kitap.isim, kitap.yazar, kitap.yayinevi, kitap.tur, kitap.baski))
+        self.baglanti.commit()
+        
     def kitapsil(self,isim):
         sorgu = "Delete From kitaplar where isim = ?"
         self.cursor.execute(sorgu,(isim,))
-        self.baglantı.commit()
-    def baskıyükselt(self,isim):
+        self.baglanti.commit()
+        
+    def baskıyukselt(self,isim):
         sorgu = "Select * From kitaplar where isim = ?"
         self.cursor.execute(sorgu,(isim,))
         kitaplar = self.cursor.fetchall()
         if (len(kitaplar) == 0):
             print("böyle bir kitap bulunmuyor...")
+            
         else:
-            baskı = kitaplar[0][4]
-            baskı += 1
-            sorgu2 = "Update kitaplar set baskı = ? where isim = ?"
-            self.cursor.execute(sorgu2,(baskı,isim))
-            self.baglantı.commit()
+            baski = kitaplar[0][4]
+            baski += 1
+            sorgu2 = "Update kitaplar set baski = ? where isim = ?"
+            self.cursor.execute(sorgu2,(baski,isim))
+            self.baglanti.commit()
 
 
 
