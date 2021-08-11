be02c4d5d0f12c9c41d5b5f35f955c674fc0b5fb
mervesenn <82107698+mervesenn@users.noreply.github.com>
2021-08-11 15:48:34 +0300

    Update hayvansınıfı.py

--- nesne tabanlı programlama/hayvansınıfı.py
+++ nesne tabanlı programlama/hayvansınıfı.py
@@ -1,77 +1,94 @@
 import time
 class Hayvanlar():
-    def __init__(self,tür="omurgasızlar ve omurgalılar",yaşamalanı="kara,hava ve su",beslenmeçeşiti="etçil,otçul veya hem etçil hem otçul",ayaksayısı="2-4 arasında değişmektedir"):
-        self.tür = tür
-        self.yaşamalanı = yaşamalanı
-        self.beslenmeçeşiti = beslenmeçeşiti
-        self.ayaksayısı = ayaksayısı
-    def bilgilerigöster(self):
+    
+    def __init__(self, tur = "omurgasızlar ve omurgalılar", yasamalani = "kara,hava ve su", beslenmecesiti = "etçil,otçul veya hem etçil hem otçul", ayaksayisi = "2-4 arasında değişmektedir"):
+        self.tur = tur
+        self.yasamalanı = yasamalani
+        self.beslenmecesiti = beslenmecesiti
+        self.ayaksayisi = ayaksayisi
+        
+    def bilgilerigoster(self):
         print("hayvanların genel özellikleri")
-        print("1)türü:{}\n2)yaşam alanı:{}\n3)beslenme çeşiti:{}\n4)ayak sayısı:{}\n".format(self.tür,self.yaşamalanı,self.beslenmeçeşiti,self.ayaksayısı))
-class Köpekler(Hayvanlar):
-    def __init__(self,tür="memeli",yaşamalanı="kara",beslenmeçeşiti="etçil",ayaksayısı="4",tipiközellikleri=["hızlıdırlar","fazla doğum yaparlar","ömürleri kısadır"],cinsleri=["Rotweiler","Dogo","Pitbull","Golden"]):
-        super().__init__(tür,yaşamalanı,beslenmeçeşiti,ayaksayısı)
+        print("1)türü:{}\n2)yaşam alanı:{}\n3)beslenme çeşiti:{}\n4)ayak sayısı:{}\n".format(self.tur, self.yasamalani, self.beslenmecesiti, self.ayaksayisi))
+        
+class Kopekler(Hayvanlar):
+    def __init__(self, tur = "memeli", yasamalani = "kara", beslenmecesiti = "etçil", ayaksayisi = "4", tipikozellikleri = ["hızlıdırlar","fazla doğum yaparlar","ömürleri kısadır"], cinsleri = ["Rotweiler","Dogo","Pitbull","Golden"]):
+        super().__init__(tur, yasamalani, beslenmecesiti, ayaksayisi)
         print("Köpek class'ının init fonksiyonu")
         self.cinsleri = cinsleri
-        self.tipiközellikleri = tipiközellikleri
-    def bilgilerigöster(self):
+        self.tipikozellikleri = tipiközellikleri
+        
+    def bilgilerigoster(self):
         print("Köpeklerin genel özellikleri")
-        print("1)türü:{}\n2)yaşam alanı:{}\n3)beslenme çeşiti:{}\n4)ayak sayısı:{}\n5)tipik özellikleri:{}\n6)cinsleri:{}\n".format(self.tür,self.yaşamalanı,self.beslenmeçeşiti,self.ayaksayısı,self.tipiközellikleri,self.cinsleri))
+        print("1)türü:{}\n2)yaşam alanı:{}\n3)beslenme çeşiti:{}\n4)ayak sayısı:{}\n5)tipik özellikleri:{}\n6)cinsleri:{}\n".format(self.tur, self.yasamalanı, self.beslenmecesiti, self.ayaksayisi, self.tipikozellikleri, self.cinsleri))
+        
     def cinsekle(self):
-        cinslistesi = input("cinleri araya virgül koyarak giriniz:")
+        cinslistesi = input("cinsleri araya virgül koyarak giriniz:")
         listeler = cinslistesi.split(",")
         for i in listeler:
             self.cinsleri.append(i)
-    def özellikekle(self):
+            
+    def ozellikekle(self):
         tipik = input("aralarına virgül koyarak özellikleri giriniz:")
         tipiklistesi = tipik.split(",")
         for i in tipiklistesi:
-            self.tipiközellikleri.append(i)
+            self.tipikozellikleri.append(i)
+            
     def __str__(self):
-        return "özellik sayısı:{}\ncins sayısı:{}\n".format(len(self.tipiközellikleri),len(self.cinsleri))
+        return "özellik sayısı:{}\ncins sayısı:{}\n".format(len(self.tipikozellikleri), len(self.cinsleri))
 
-class Kuşlar(Hayvanlar):
-    def __init__(self,tür="memeli",yaşamalanı="hava ve kara",beslenmeçeşiti="etçil",ayaksayısı="2",tipiközellikleri=["uçarlar","hızlıdırlar","uçma mesafeleri 10km çıkabilir"],cinsleri=["Muhabbet Kuşu","Sultan Papağanı","Hint Bülbülü","Amazon Papağanı"]):
-        super().__init__(tür,yaşamalanı,beslenmeçeşiti,ayaksayısı)
+class Kuslar(Hayvanlar):
+    def __init__(self, tur = "memeli", yasamalani = "hava ve kara", beslenmecesiti = "etçil", ayaksayisi = "2", tipikozellikleri = ["uçarlar","hızlıdırlar","uçma mesafeleri 10km çıkabilir"], cinsleri = ["Muhabbet Kuşu","Sultan Papağanı","Hint Bülbülü","Amazon Papağanı"]):
+        super().__init__(tur, yasamalani, beslenmecesiti, ayaksayisi)
         print("Kuşların init fonksiyonu")
         self.cinsleri = cinsleri
-        self.tipiközellikleri = tipiközellikleri
-    def bilgilerigöster(self):
+        self.tipikozellikleri = tipikozellikleri
+        
+    def bilgilerigoster(self):
         print("Kuşların genel özellikleri")
-        print("1)türü:{}\n2)yaşam alanı:{}\n3)beslenme çeşiti:{}\n4)ayak sayısı:{}\n5)tipik özellikleri:{}\n6)cinsleri:{}\n".format(self.tür,self.yaşamalanı,self.beslenmeçeşiti,self.ayaksayısı,self.tipiközellikleri,self.cinsleri))
+        print("1)türü:{}\n2)yaşam alanı:{}\n3)beslenme çeşiti:{}\n4)ayak sayısı:{}\n5)tipik özellikleri:{}\n6)cinsleri:{}\n".format(self.tur, self.yasamalani, self.beslenmecesiti, self.ayaksayisi, self.tipikozellikleri, self.cinsleri))
+        
     def cinsekle(self):
         cinslistesi = input("cinsleri araya virgül koyarak giriniz:")
         listeler = cinslistesi.split(",")
         for i in listeler:
             self.cinsleri.append(i)
-    def özellikekle(self):
+            
+    def ozellikekle(self):
         tipik = input("aralarına virgül koyarak özellikleri giriniz:")
         tipiklistesi = tipik.split(",")
         for i in tipiklistesi:
-            self.tipiközellikleri.append(i)
+            self.tipikozellikleri.append(i)
+            
     def __str__(self):
-        return "özellik sayısı:{}\ncins sayısı:{}\n".format(len(self.tipiközellikleri),len(self.cinsleri))
+        return "özellik sayısı:{}\ncins sayısı:{}\n".format(len(self.tipikozellikleri), len(self.cinsleri))
+    
 class Atlar(Hayvanlar):
-    def __init__(self,tür="memeli",yaşamalanı="kara",beslenmeçeşiti="otçul",ayaksayısı="4",tipiközellikleri=["hızlıdırlar","nalları vardır","boyları uzundur"],cinsleri=["Dole Pony","Arap Atı","İnhiliz Atı"]):
-        super().__init__(tür,yaşamalanı,beslenmeçeşiti,ayaksayısı)
+    def __init__(self, tur = "memeli", yasamalani = "kara", beslenmecesiti = "otçul", ayaksayisi = "4", tipikozellikleri = ["hızlıdırlar","nalları vardır","boyları uzundur"], cinsleri = ["Dole Pony","Arap Atı","İnhiliz Atı"]):
+        super().__init__(tur, yasamalani, beslenmecesiti, ayaksayisi)
         print("Atlar class'ının init fonksiyonu")
         self.cinsleri = cinsleri
-        self.tipiközellikleri = tipiközellikleri
-    def bilgilerigöster(self):
+        self.tipikozellikleri = tipikozellikleri
+        
+    def bilgilerigoster(self):
         print("Atların genel özellikleri")
-        print("1)türü:{}\n2)yaşam alanı:{}\n3)beslenme çeşiti:{}\n4)ayak sayısı:{}\n5)tipik özellikleri:{}\n6)cinsleri:{}\n".format(self.tür,self.yaşamalanı,self.beslenmeçeşiti,self.ayaksayısı,self.tipiközellikleri,self.cinsleri))
+        print("1)türü:{}\n2)yaşam alanı:{}\n3)beslenme çeşiti:{}\n4)ayak sayısı:{}\n5)tipik özellikleri:{}\n6)cinsleri:{}\n".format(self.tur, self.yasamalani, self.beslenmecesiti, self.ayaksayisi, self.tipikozellikleri, self.cinsleri))
+        
     def cinsekle(self):
         cinslistesi = input("cinsleri araya virgül koyarak giriniz:")
         listeler = cinslistesi.split(",")
         for i in listeler:
             self.cinsleri.append(i)
-    def özellikekle(self):
+            
+    def ozellikekle(self):
         tipik = input("aralarına virgül koyarak özellikleri giriniz:")
         tipiklistesi = tipik.split(",")
         for i in tipiklistesi:
-            self.tipiközellikleri.append(i)
+            self.tipikozellikleri.append(i)
+            
     def __str__(self):
-        return "özellik sayısı:{}\ncins sayısı:{}\n".format(len(self.tipiközellikleri),len(self.cinsleri))
+        return "özellik sayısı:{}\ncins sayısı:{}\n".format(len(self.tipikozellikleri), len(self.cinsleri))
+    
 while True:
     print("""
     ****************
@@ -83,52 +100,57 @@ while True:
     4)atların genel özellikleri
     5)çıkış
     """)
-    seçim=input("işleminizi giriniz:")
-    if seçim == "1":
+    
+    secim = input("işleminizi giriniz:")
+    if secim == "1":
         hayvanlar = Hayvanlar()
-        hayvanlar.bilgilerigöster()
-    elif seçim == "2":
-        köpekler = Köpekler()
-        köpekler.bilgilerigöster()
+        hayvanlar.bilgilerigoster()
+        
+    elif secim == "2":
+        kopekler = Kopekler()
+        kopekler.bilgilerigoster()
         time.sleep(1)
         sel = input("1)cins ekleme\n2)özellik ekleme\n3)özellik ve cins sayıları\n4)çıkış\n")
         if sel == "1":
-            köpekler.cinsekle()
+            kopekler.cinsekle()
         elif sel == "2":
-            köpekler.özellikekle()
+            kopekler.ozellikekle()
         elif sel == "3":
-            print(köpekler)
+            print(kopekler)
         else:
             print("bu menüden çıkış yapılıyor")
             time.sleep(1)
-    elif seçim == "3":
-        kuşlar = Kuşlar()
-        kuşlar.bilgilerigöster()
+            
+    elif secim == "3":
+        kuslar = Kuslar()
+        kuslar.bilgilerigoster()
         time.sleep(1)
         sel = input("1)cins ekleme\n2)özellik ekleme\n3)özellik ve cins sayıları\n4)çıkış\n")
         if sel == "1":
-            kuşlar.cinsekle()
+            kuslar.cinsekle()
         elif sel == "2":
-            kuşlar.özellikekle()
+            kuslar.ozellikekle()
         elif sel == "3":
-            print(kuşlar)
+            print(kuslar)
         else:
             print("bu menüden çıkış yapılıyor")
             time.sleep(1)
-    elif seçim == "4":
+            
+    elif secim == "4":
         atlar = Atlar()
-        atlar.bilgilerigöster()
+        atlar.bilgilerigoster()
         time.sleep(1)
         sel = input("1)cins ekleme\n2)özellik ekleme\n3)özellik ve cins sayıları\n4)çıkış\n")
         if sel == "1":
             atlar.cinsekle()
         elif sel == "2":
-            atlar.özellikekle()
+            atlar.ozellikekle()
         elif sel == "3":
             print(atlar)
         else:
             print("bu menüden çıkış yapılıyor")
             time.sleep(1)
+            
     else:
         print("işleminiz sonlandırılıyor..")
         time.sleep(1)
