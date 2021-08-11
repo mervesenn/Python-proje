bcfa17daefbfcdf2ef76080b9684468d32b5f275
mervesenn <82107698+mervesenn@users.noreply.github.com>
2021-08-11 15:32:31 +0300

    Update bilgisayarsınıfı.py

--- nesne tabanlı programlama/bilgisayarsınıfı.py
+++ nesne tabanlı programlama/bilgisayarsınıfı.py
@@ -2,52 +2,62 @@ import time
 import random
 
 class Bilgisayar():
-    def __init__(self,bilgisayardurum="kapalı",bilgisayarses=13,ekranparlaklık=5):
-        self.bilgisayardurum=bilgisayardurum
-        self.bilgisayarses=bilgisayarses
-        self.ekranparlaklık=ekranparlaklık
-    def bilgisayaraç(self):
-        if (self.bilgisayardurum=="açık"):
+    def __init__(self, bilgisayardurum = "kapalı", bilgisayarses=13, ekranparlaklik=5):
+        self.bilgisayardurum = bilgisayardurum
+        self.bilgisayarses = bilgisayarses
+        self.ekranparlaklik = ekranparlaklik
+        
+    def bilgisayarac(self):
+        if (self.bilgisayardurum == "açık"):
             print("bilgisayar zaten açık")
+            
         else:
             print("bilgisayar açılıyor..")
-            self.bilgisayardurum="açık"
+            self.bilgisayardurum = "açık"
+            
     def bilgisayarkapat(self):
-        if (self.bilgisayardurum=="kapalı"):
+        if (self.bilgisayardurum == "kapalı"):
             print("bilgisayar zaten kapalı")
+            
         else:
             print("bilgisayar kapanıyor..")
-            self.bilgisayardurum="kapalı"
-    def sesayarı(self):
+            self.bilgisayardurum = "kapalı"
+            
+    def sesayari(self):
         while True:
             cevap = input("ses azaltmak için f7'e basın:'f7'\nsesi artırmak için f8'e basın:'f8'\nçıkış:çıkış")
-            if (cevap=='f7'):
-                if(self.bilgisayarses!=0):
-                    self.bilgisayarses-=1
-                    print("ses:",self.bilgisayarses)
-            elif(cevap=='f8'):
-                if(self.bilgisayarses!=100):
-                    self.bilgisayarses+=1
-                    print("ses:",self.bilgisayarses)
+            if (cevap =='f7'):
+                if(self.bilgisayarses!= 0):
+                    self.bilgisayarses -= 1
+                    print("ses:", self.bilgisayarses)
+                    
+            elif(cevap == 'f8'):
+                if(self.bilgisayarses != 100):
+                    self.bilgisayarses += 1
+                    print("ses:", self.bilgisayarses)
+                          
             else:
-                print("ses güncellendi:",self.bilgisayarses)
+                print("ses güncellendi:", self.bilgisayarses)
                 break
-    def parlaklık(self):
+                          
+    def parlaklik(self):
         while True:
             cevap = input("parlaklığı arttır:'>'\nparlaklığı azalt:'<'\nçıkış:çıkış")
-            if (cevap=='>'):
-                if(self.ekranparlaklık!=10):
-                    self.ekranparlaklık+=1
-                    print("ekranparlaklığı:",self.ekranparlaklık)
-            elif(cevap=='<'):
-                if(self.ekranparlaklık!=0):
-                    self.ekranparlaklık-=1
-                    print("ekranparlaklığı:",self.ekranparlaklık)
+            if (cevap == '>'):
+                if(self.ekranparlaklik != 10):
+                    self.ekranparlaklik += 1
+                    print("ekranparlaklığı:", self.ekranparlaklik)
+            elif(cevap == '<'):
+                if(self.ekranparlaklik != 0):
+                    self.ekranparlaklik -= 1
+                    print("ekranparlaklığı:",self.ekranparlaklik)
             else:
-                print("ekran parlaklığı güncellendi:",self.ekranparlaklık)
+                print("ekran parlaklığı güncellendi:", self.ekranparlaklik)
                 break
+                          
     def __str__(self):
-        return "bilgisayar durumu:{}\nbilgisayar ses:{}\nbilgisayar ekran parlaklığı:{}\n".format(self.bilgisayardurum,self.bilgisayarses,self.ekranparlaklık)
+        return "bilgisayar durumu:{}\nbilgisayar ses:{}\nbilgisayar ekran parlaklığı:{}\n".format(self.bilgisayardurum, self.bilgisayarses, self.ekranparlaklik)
+                          
 bilgisayar = Bilgisayar()
 print("""
 bilgisayar
@@ -60,19 +70,25 @@ bilgisayar
 """)
 
 while True:
-    işlem=input("işlemi seçiniz:")
-    if (işlem=="q"):
+    islem = input("işlemi seçiniz:")
+    if (islem == "q"):
         print("program sonlandırılıyor..")
         break
-    elif (işlem=="1"):
-        bilgisayar.bilgisayaraç()
-    elif (işlem=="2"):
+                          
+    elif (islem == "1"):
+        bilgisayar.bilgisayarac()
+                          
+    elif (islem == "2"):
         bilgisayar.bilgisayarkapat()
-    elif (işlem=="3"):
-        bilgisayar.sesayarı()
-    elif (işlem=="4"):
-        bilgisayar.parlaklık()
-    elif (işlem=="5"):
+                          
+    elif (islem == "3"):
+        bilgisayar.sesayari()
+                          
+    elif (islem == "4"):
+        bilgisayar.parlaklik()
+                          
+    elif (islem == "5"):
         print(bilgisayar)
+                          
     else:
         print("geçersiz işlem!")
